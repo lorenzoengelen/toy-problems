bd9e15fa7c270e40bbf540da6fadcfb5968ea173
Willson Mock <willson.mock@gmail.com>
2015-08-19 14:15:42 -0700

    Complete balancedParens.js

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,38 @@
  *
  */
 
-var balancedParens = function (input) {
+var isInput = function (text) {
+  return text === '(' ||
+         text === '[' ||
+         text === '{';
+};
 
+var isOutput = function (text) {
+  return text === ')' ||
+         text === ']' ||
+         text === '}';
+};
+
+var map = {
+  ')' : '(',
+  ']' : '[',
+  '}' : '{'
+};
+
+var balancedParens = function (input) {
+  var stack = [];
+  var split = input.split('');
+  var len = split.length;
+
+  for (var i = 0; i < len; i += 1) {
+    var cur = split[i];
+    if (isInput(cur)) {
+      stack.push(cur);
+    } else if (isOutput(cur)) {
+      var popped = stack.pop();
+      if (popped !== map[cur]) return false;
+    }
+  }
+
+  return stack.length === 0;
 };
