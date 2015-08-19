38c2bc03f206cdc528e3ac598df6f63adee46c71
Willson Mock <willson.mock@gmail.com>
2015-08-19 14:02:46 -0700

    Complete rockPaperScissors.js

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -24,6 +24,17 @@
 *
 */
 
-var rockPaperScissors = function () {
-  // TODO: your solution here
+var rockPaperScissors = function (numRounds) {
+  if (numRounds  <= 0) throw new Error('The number of rounds must be a positive number.');
+  if (numRounds === 1) return [['rock'], ['paper'], ['scissors']];
+
+  var possibleHands = ['rock', 'paper', 'scissors'];
+  var previous = rockPaperScissors(numRounds - 1);
+
+  return previous.reduce(function (acc, cur) {
+    possibleHands.forEach(function (hand) {
+      acc.push(cur.concat(hand));
+    });
+    return acc;
+  }, []);
 };
