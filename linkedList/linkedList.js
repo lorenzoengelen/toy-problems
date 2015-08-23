262e7bec79e2d66b160b8167dbaeb97aeeb9b0f3
Willson Mock <willson.mock@gmail.com>
2015-08-23 15:59:35 -0700

    Complete linkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -13,27 +13,53 @@
 // list.tail;        //yields 'null';
 
 var LinkedList = function () {
-  
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
 
 LinkedList.prototype.addToTail = function (value) {
-  
+  var node = this.makeNode(value);
+  if (this.isEmpty()) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
 };
 
 LinkedList.prototype.removeHead = function () {
-  
+  if (!this.isEmpty()) {
+    var head = this.head;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+    head.next = null;
+    return head;
+  }
 };
 
 LinkedList.prototype.contains = function (value, node) {
-  
+  var start = (node === void 0) ? this.head : node;
+  while (start) {
+    if (start.value === value) return true;
+    start = start.next;
+  }
+  return false;
 };
 
 LinkedList.prototype.isEmpty = function () {
-  
+  return this.head === null && this.tail === null;
 };
 
 LinkedList.prototype.makeNode = function (value) {
-  
+  return {
+    value: value,
+    next: null
+  };
 };
