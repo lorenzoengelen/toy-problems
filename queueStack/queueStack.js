24090fe2353fb24fa9cd7c8e02eb6b3a7421abe4
Willson Mock <willson.mock@gmail.com>
2015-08-23 16:10:26 -0700

    Complete queueStack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,21 +7,29 @@
   * Stack Class
   */
 
-var Stack = function() {
+var Stack = function () {
+  var storage = {};
+  var top = 0;
   
   // add an item to the top of the stack
   this.push = function (value) {
-  
+    storage[top] = value;
+    top += 1;
   };
 
   // remove an item from the top of the stack
   this.pop = function () {
-  
+    if (this.size() > 0) {
+      var remove = storage[top - 1];
+      delete storage[top - 1];
+      top -= 1;
+      return remove;
+    }
   };
 
   // return the number of items in the stack
   this.size = function () {
-  
+    return top;
   };
 
   return this;
@@ -30,23 +38,36 @@ var Stack = function() {
 /**
   * Queue Class
   */
-var Queue = function() {
+var Queue = function () {
   // Use two `stack` instances to implement your `queue` Class
   var inbox = new Stack();
   var outbox = new Stack();
 
   // called to add an item to the `queue`
   this.enqueue = function (value) {
-  
+    inbox.push(value);
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function () {
-  
+    var popped;
+    while (inbox.size() > 0) {
+      popped = inbox.pop();
+      outbox.push(popped);
+    }
+
+    var result = outbox.pop();
+
+    while (outbox.size() > 0) {
+      popped = outbox.pop();
+      inbox.push(popped);
+    }
+
+    return result;
   };
 
   // should return the number of items in the queue
   this.size = function () {
-  
+    return inbox.size();
   };
 };
