aa9a6d9a492f7ca212650f73a0a73d000f17e41e
Willson Mock <willson.mock@gmail.com>
2015-08-23 13:50:27 -0700

    Complete constantTimeStackMin

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -6,23 +6,48 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+var Stack = function () {
+  var storage = {};
+  var minStorage = {};
+  var top = 0;
+  var minTop = 0;
 
   // add an item to the top of the stack
-  this.push = function(value){
+  this.push = function (value) {
+    storage[top] = value;
+    top += 1;
+
+    if (minTop === 0 || (minTop > 0 && minStorage[minTop - 1] >= value)) {
+      minStorage[minTop] = value;
+      minTop += 1;
+    }
   };
 
   // remove an item from the top of the stack
-  this.pop = function(){
+  this.pop = function (){
+    if (this.size() > 0) {
+      var remove = storage[top - 1];
+      delete storage[top - 1];
+      top -= 1;
+
+      if (minStorage[minTop - 1] === remove) {
+        delete minStorage[minTop - 1];
+        minTop -= 1;
+      }
+
+      return remove;
+    }
   };
 
   // return the number of items in the stack
-  this.size = function(){
-  }
+  this.size = function () {
+    return top;
+  };
 
-  this.min = function() {
+  this.min = function () {
+    return minStorage[minTop - 1];
+  };
 
-  }
   return this;
 };
 
