261359c6d53cd402aecc373525870c4240203b23
Willson Mock <willson.mock@gmail.com>
2015-08-23 16:18:02 -0700

    Complete reverseArray

--- reverseArray/reverseArray.js
+++ reverseArray/reverseArray.js
@@ -11,5 +11,28 @@
  *   reverseArray([1, 8, 39, null, 2, 9, 'bob'])[0] // should equal => 'bob'
  */
 
-var reverseArray = function(array) {
+var reverseArray = function (array) {
+  var result = [];
+  while (array.length > 0) {
+    result.push(array.pop());
+  }
+  return result;
+};
+
+var reverseArray = function (array) {
+  var start = 0;
+  var end = array.length - 1;
+
+  while (start < end) {
+    swap(array, start, end);
+    start += 1;
+    end -= 1;
+  }
+  return array;
+};
+
+var swap = function (array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 };
