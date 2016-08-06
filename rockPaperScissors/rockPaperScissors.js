ae9adcd2aa48ff88997011454284a262e9d26eb0
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-08-06 16:54:31 -0700

    Clean toy problem branch

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -25,16 +25,4 @@
 */
 
 var rockPaperScissors = function (numRounds) {
-  if (numRounds  <= 0) throw new Error('The number of rounds must be a positive number.');
-  if (numRounds === 1) return [['rock'], ['paper'], ['scissors']];
-
-  var possibleHands = ['rock', 'paper', 'scissors'];
-  var previous = rockPaperScissors(numRounds - 1);
-
-  return previous.reduce(function (acc, cur) {
-    possibleHands.forEach(function (hand) {
-      acc.push(cur.concat(hand));
-    });
-    return acc;
-  }, []);
 };
